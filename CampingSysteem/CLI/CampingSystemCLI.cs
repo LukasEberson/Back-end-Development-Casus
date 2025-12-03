a84522f087c491db707239bcefec73ce30afb08c
Maurice Merry <mauricemerry.1998@gmail.com>
2025-12-03 19:52:49 +0100

    begin aan camping systeem cli

--- CampingSysteem/CLI/CampingSystemCLI.cs
+++ CampingSysteem/CLI/CampingSystemCLI.cs
@@ -4,6 +4,10 @@ namespace CommandLineInterface
     {
         public static void Run()
         {
+            CommandLineProgram program = new CommandLineProgram("Camping Systeem");
+            MainMenuScreen mainMenu = new MainMenuScreen(program);
+
+            program.Run(mainMenu);
         }
     }
 }
